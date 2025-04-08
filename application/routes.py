83a784ee003c42ec922accdb37803752da861cdb
Emma Tudor <emma.tudor1995@gmail.com>
2025-04-08 16:30:03 +0100

    CREATE: Wonderland

--- application/routes.py
+++ application/routes.py
@@ -2,11 +2,63 @@ from flask import render_template, url_for, request, redirect
 
 from application import app
 
-
 @app.route('/')
 def home_page():
-    return render_template('home.html', title_head='Enchanted Getaway Travels', title_body='Enchanted Getaway Travels', subtitle='★ Travel the unexplored lands ★', img="static/images/wallpaper_home.jpeg")
+    return render_template('home.html', title_head='Enchanted Getaways', title_body='Enchanted Getaways', subtitle='★ Travel the unexplored lands ★', img="static/images/wallpaper_home.jpeg")
 
-# @app.route('/')
-# def home():
-#     return render_template()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@app.route('/wonderland')
+def wonderland():
+    return render_template('destination1.html', title_head='Wonderland', title_body='Wonderland', subtitle='Want to wander in Wonderland?', img='static/images/WT.jpg')
+
+
