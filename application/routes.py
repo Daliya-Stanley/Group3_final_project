27993c829f16fdf02f057b8c26b68d129df8a3b5
Daphne Obadan <daphneobadan@gmail.com>
2025-04-08 14:15:23 +0100

    Routes added on lines given

--- application/routes.py
+++ application/routes.py
@@ -3,13 +3,14 @@ from application.forms.register_form import RegistrationForm
 from application.forms.login_form import LoginForm
 from application import app
 
-
 @app.route('/')
 @app.route('/home')
 def home_page():
-    return render_template('home.html', title_head='Enchanted Getaway Travels', title_body='Enchanted Getaway Travels', subtitle='★ Travel the unexplored lands ★', img="static/images/wallpaper_home.jpeg")
+    return render_template('home.html', title_head='Enchanted Getaway Travels',
+                           title_body='Enchanted Getaway Travels',
+                           subtitle='★ Travel the unexplored lands ★',
+                           img="static/images/wallpaper_home.jpeg")
 
-<<<<<<< HEAD
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     register_form = RegistrationForm()
@@ -20,14 +21,16 @@ def register():
         email = register_form.email.data
         password = register_form.password.data
 
-        #add_person(first_name, last_name, password)
+        # Here you would call a function to add the user, e.g.:
+        # add_person(username, email, password)
+
         return redirect(url_for('welcome'))
 
     return render_template('register.html',
                            form=register_form,
                            message=error,
-                           title_head='register',
-                           title_body='register',
+                           title_head='Register',
+                           title_body='Register',
                            img="")
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -38,13 +41,13 @@ def login():
     if request.method == 'POST':
         username = login_form.username.data
         password = login_form.password.data
-
+        # Logic to verify the user would go here
+        # If successful, redirect to a welcome page or dashboard
 
     return render_template('login.html',
                            form=login_form,
                            message=error,
-                           title_head='login',
-                           )
+                           title_head='Login')
 
 @app.route('/welcome', methods=['GET', 'POST'])
 def welcome():
@@ -53,56 +56,6 @@ def welcome():
 @app.route('/game', methods=['GET', 'POST'])
 def game():
     return render_template('game.html')
-=======
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -117,16 +70,9 @@ def game():
 @app.route("/cinderella_kingdom")
 def cinderella_kingdom():
     return render_template(
-        "cinderellas_kingdom.html", title_head="Cinderella's Kingdom", title_body='Far Far Away!', subtitle='★ Welcome to the destination where wishes come true ★', img="images/cinderella.jpg"
+        "cinderellas_kingdom.html",
+        title_head="Cinderella's Kingdom",
+        title_body='Far Far Away!',
+        subtitle='★ Welcome to the destination where wishes come true ★',
+        img="images/cinderella.jpg"
     )
-
-
-
-
-
-
-
-
-
-
->>>>>>> Daphne
