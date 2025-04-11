d57d3f93353f8c8de477ff29a20ceb73a5a9aca5
Emma Tudor <emma.tudor1995@gmail.com>
2025-04-11 21:27:26 +0100

    inserted into tables

--- database/Destination table.sql
+++ database/Destination table.sql
@@ -6,3 +6,11 @@ DestinationID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 DestinationName VARCHAR (25) NOT NULL,
 DestinationPricePerNight INT NOT NULL
 );
+
+INSERT INTO Destination (DestinationName, DestinationPricePerNight)
+VALUES 
+('Wonderland','£100'),
+('Cinderella','£125'),
+('Destination 3','£100'),
+('Frozen','£100'),
+('Destination 5','£150');
--- database/ProductStatus table.sql
+++ database/ProductStatus table.sql
@@ -5,3 +5,8 @@ create table ProductStatus
 ProductStatusID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 Status VARCHAR (20) NOT NULL
 );
+
+INSERT INTO ProductStatus (Status)
+VALUES 
+('Available'),
+('Not Available');
