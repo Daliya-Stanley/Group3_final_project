e6febc36d04fddba2737d90c3442feeb9bc6a1bc
Emma Tudor <emma.tudor1995@gmail.com>
2025-04-11 21:24:03 +0100

    created draft stored procedures

---
+++ database/Stored Procedures Draft.sql
@@ -0,0 +1,53 @@
+Stored Procedures:
+
+DELIMITER //
+
+CREATE PROCEDURE pRetrieve_All_Users()
+BEGIN
+SELECT * FROM User;
+END //
+
+DELIMITER ;
+
+DELIMITER //
+
+CREATE PROCEDURE pRetrieve_All_Bookings()
+BEGIN
+SELECT * FROM Booking;
+END //
+
+DELIMITER ;
+
+DELIMITER // 
+
+CREATE PROCEDURE pGet_Available_Products(
+IN pStatus varchar(25)
+)
+BEGIN
+SELECT * FROM ProductStatus 
+WHERE Status = Available;
+END //
+
+DELIMITER ;
+
+DELIMITER //
+
+CREATE PROCEDURE pGet_Booked_Experiences(
+IN pDateReserved DATE
+)
+BEGIN
+SELECT * FROM Experiences 
+WHERE DateReserved = pDateReserved;
+END //
+
+DELIMITER ;
+
+DELIMITER //
+
+CREATE PROCEDURE pRetrieve_Products_Purchased()
+BEGIN
+SELECT * FROM Shopping;
+END //
+
+DELIMITER ;
+
