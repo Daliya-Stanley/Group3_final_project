573024e0d6605373f5204303ebc05e03b03c57ef
Emma Tudor <emma.tudor1995@gmail.com>
2025-04-11 15:13:52 +0100

    Added inserts to tables, not executed currently

--- database/Experience table.sql
+++ database/Experience table.sql
@@ -7,3 +7,11 @@ ExperienceName VARCHAR (50) NOT NULL,
 ExperiencePrice INT NOT NULL,
 DateReserved date NULL
 );
+
+INSERT INTO Experiences (ExperienceName, ExperiencePrice, DateReserved)
+VALUES 
+('Make a Snowman','£50'),
+('Flying Lesson','£250'),
+('Troll Party','£50'),
+('Scuba Diving','£250'),
+('Spa Experience','£100');
--- database/Product table.sql
+++ database/Product table.sql
@@ -11,3 +11,11 @@ foreign key (ProductStatusID) references ProductStatus(ProductStatusID)
 
 alter table Product
 add column ProductImage VARCHAR (50) NOT NULL;
+
+INSERT INTO Product (ProductName, ProductPrice, ProductImage)
+VALUES 
+('All You Can Pack Suitcase','£200', 'product_suitcase.jpeg'),
+('Magical Carpet','£500', 'product_carpet.jpeg'),
+('Magic Potion','£100', 'product-magic bottle.jpeg'),
+('Magic Wand','£50', 'magical_wand.jpeg'),
+('Spectacular One Dress - No Mess','£100', 'product_clothes.jpeg');
